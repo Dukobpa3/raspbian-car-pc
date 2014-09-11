e50992d24012496a4c020ac457647a19dfab2373
Dukobpa3 <a.burdun@gmail.com>
2014-09-11 16:36:27 +0300

    change colors

--- raspbian/bash/mediaserver.sh
+++ raspbian/bash/mediaserver.sh
@@ -3,25 +3,25 @@
 . ./include/vars.sh
 
 function setupMediaServer {
-    echo "$(tput setaf 6)[+] Setup mediaserver... $(tput sgr 0)"
+    echo "$(tput setaf 5)[+] Setup mediaserver... $(tput sgr 0)"
 
     ##-------------------------------------
-    echo "$(tput setaf 7)[+] Configure automount usb sticks... $(tput sgr 0)"
-    echo "$(tput setaf 7)[+] Install soft... $(tput sgr 0)"
+    echo "$(tput setaf 6)[+] Configure automount usb sticks... $(tput sgr 0)"
+    echo "$(tput setaf 6)[+] Install soft... $(tput sgr 0)"
     apt-get install -y ntfs-3g usbmount
 
-    echo "$(tput setaf 7)[+] Add utf-8... $(tput sgr 0)"
+    echo "$(tput setaf 6)[+] Add utf-8... $(tput sgr 0)"
     echo "$(tput setaf 1)[+] Replacing config by version from repo!!! $(tput sgr 0)"
     cat ./etc/usbmount/usbmount.conf > /etc/usbmount/usbmount.conf
 
     ##-------------------------------------
-    echo "$(tput setaf 7)[+] Install UPnP server... $(tput sgr 0)"
+    echo "$(tput setaf 6)[+] Install UPnP server... $(tput sgr 0)"
     apt-get instal -y minidlna
 
     #TODO add config editing, or copy current
 
     ##-------------------------------------
-    echo "$(tput setaf 7)[+] Link usb automounts to minidlna default folders... $(tput sgr 0)"
+    echo "$(tput setaf 6)[+] Link usb automounts to minidlna default folders... $(tput sgr 0)"
     ln -s /media /var/lib/minidlna/media
 
     return ${RETOK}
