6f3e14bb69581567c96cceb2d4e1f17ccb3623bf
Dukobpa3 <a.burdun@gmail.com>
2014-09-11 22:21:48 +0300

    update settings

--- raspbian/bash/setup.sh
+++ raspbian/bash/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# version 1.0.0
+
 #$(tput setaf 1) - Red - Warning
 #$(tput setaf 2) - Green - Prompt
 #$(tput setaf 3) - Yellow - Important info
@@ -23,12 +25,45 @@ function checkVariables {
     #TODO check all filled
     #TODO check wifi dongle name
     #TODO check currently installed versions of soft and settings
+
+    echo "Check WiFi settings"
+    if [ ${SETUP_WIFI} ]; then
+        echo "configure WiFi"
+    else
+        echo "don't configure WiFi"
+    fi
+
+
+    echo "Check UPnP server settings"
+    if [ ${SETUP_DLNA_SERVER} ]; then
+        echo "configure UPnP server"
+    else
+        echo "don't configure UPnP server"
+    fi
+
+
+    echo "Check UPnP player settings"
+    if [ ${SETUP_DLNA_PLAYER} ]; then
+        echo "configure UPnP player"
+    else
+        echo "don't configure UPnP player"
+    fi
+
     return 0
 }
 
-#TODO if [ checkVariables != 0 ] return
-checkVariables
+function checkCurrentState {
 
+    return 0
+}
+checkVariables
+if [ $? == 1 ]; then
+    echo "$(tput setaf 1)Error in your config file $(tput sgr 0)"
+    exit 1
+fi
+
+checkCurrentState
+echo "Current state is: " $?
 
 ## All is ok. Start setup
 echo "$(tput setaf 6)Import lan config... $(tput sgr 0)"
--- raspbian/bash/template.VARIABLES.sh
+++ raspbian/bash/template.VARIABLES.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# WiFi dongle ID. If empty - lan settings will be ignored
-WLAN_DEVICE="wlan0"
+# setup modules checker
+SETUP_WIFI=yes
+SETUP_DLNA_SERVER=yes
+SETUP_DLNA_PLAYER=yes
+
 PC_NAME="MediaAP"
+
+# WiFi dongle ID.
+# Should be setted up if SETUP_WIFI=yes
+WLAN_DEVICE="wlan0"
 
 # Access point or Client settings for WiFi adapter
 # If selected 1 - you'll get an new WiFi network.
@@ -11,12 +18,23 @@ PC_NAME="MediaAP"
 # With set as Client you can add new credentials by a command:
 # sudo wpa_passphrase "your ssid" "your pass to ssid" >> /etc/wpa_supplicant/wpa_supplicant.conf
 # 1 - AP; 2 - Client;
+# Should be setted up if SETUP_WIFI=yes
 WIFI_TYPE=1
 
+# Should be setted up if SETUP_WIFI=yes
 AP_NAME="${PC_NAME}-network"
+
+# Should be setted up if SETUP_DLNA_SERVER=yes
 UPNP_SERVER_NAME="${PC_NAME}-library"
+
+USB_AUTOMOUNT_STICKS_NUM=2
+
+# Sound config (currently not workin)
+# 1 - alsa; 2 - pulseaudio
+SOUND_CONFIG=1
 
 # UPnP player.
 # 1 - upmpdcli; 2 - MediaPlayer; 3 - gmediarender-resurrect.
+# Should be setted up if SETUP_DLNA_PLAYER=yes
 UPNP_PLAYER=1
 UPNP_PLAYER_NAME="${PC_NAME}-player"
